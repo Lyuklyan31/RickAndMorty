068b8fc7b6c1cc3b14615785226f12c02339ed4e
Oleh <lyuclyan.oleg14@gmail.com>
2024-11-11 17:17:09 +0200

    Added pagination to List.

--- RickAndMorty/RickAndMorty/MainContenView/SubViews/ListCharactersView.swift
+++ RickAndMorty/RickAndMorty/MainContenView/SubViews/ListCharactersView.swift
@@ -10,8 +10,22 @@ struct ListCharactersView: View {
             LazyVStack {
                 ForEach(viewModel.characters) { character in
                     CharacterCellView(character: character)
+                        .onAppear {
+                            if character == viewModel.characters.last {
+                                viewModel.loadMoreCharacters()
+                            }
+                        }
                 }
             }
         }
+        if viewModel.characters.isEmpty {
+            ProgressView()
+        }
+    }
+}
+
+extension RMCharacterModel: @retroactive Equatable {
+    public static func == (lhs: RMCharacterModel, rhs: RMCharacterModel) -> Bool {
+        return lhs.id == rhs.id
     }
 }
