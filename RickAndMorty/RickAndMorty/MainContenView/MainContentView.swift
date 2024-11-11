9111f216b6566e0855c22d8c4c51fcf0c83077cd
Oleh <lyuclyan.oleg14@gmail.com>
2024-11-11 17:16:28 +0200

    Added alert.

--- RickAndMorty/RickAndMorty/MainContenView/MainContentView.swift
+++ RickAndMorty/RickAndMorty/MainContenView/MainContentView.swift
@@ -3,15 +3,28 @@ import RickMortySwiftApi
 
 struct MainContentView: View {
     @ObservedObject var viewModel = MainViewModel()
-    
+    @State private var showingAlert = false
+
     var body: some View {
         NavigationView {
             ZStack {
                 Color.background.edgesIgnoringSafeArea(.all)
-                   
+
                 ListCharactersView(viewModel: viewModel)
             }
             .navigationTitle("Characters")
+            .onChange(of: viewModel.alertMessage) {
+                if viewModel.alertMessage != nil {
+                    showingAlert = true
+                }
+            }
+            .alert(isPresented: $showingAlert) {
+                Alert(
+                    title: Text("Error"),
+                    message: Text(viewModel.alertMessage ?? ""),
+                    dismissButton: .default(Text("OK"))
+                )
+            }
         }
     }
 }
