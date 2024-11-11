c5eab7ceaf766da36126bbdb9d0fa75b8f42f1be
Oleh <lyuclyan.oleg14@gmail.com>
2024-11-11 04:38:18 +0200

    Created CharacterHeaderView.

---
+++ RickAndMorty/RickAndMorty/CharacterPageView/SubViews/CharacterHeaderView.swift
@@ -0,0 +1,56 @@
+//
+//  CharacterHeaderView.swift
+//  RickAndMorty
+//
+//  Created by Mac on 11.11.2024.
+//
+
+import SwiftUI
+import RickMortySwiftApi
+import Kingfisher
+
+struct CharacterHeaderView: View {
+    let character: RMCharacterModel
+    
+    private var genderColor: Color {
+        character.gender == "Male" ? Color.maleBlue : Color.femalePink
+    }
+    
+    private var genderBackgroundColor: Color {
+        genderColor.opacity(0.1)
+    }
+    
+    var body: some View {
+        VStack {
+            KFImage(URL(string: character.image))
+                .resizable()
+                .scaledToFill()
+                .cornerRadius(13)
+                .frame(height: 343)
+                .padding(.top, 10)
+                .padding()
+            
+            HStack {
+                Text(character.name)
+                    .font(.system(size: 24, weight: .semibold))
+                
+                Spacer()
+                
+                Text(character.gender)
+                    .font(.system(size: 14))
+                    .foregroundColor(genderColor)
+                    .padding(.vertical, 4)
+                    .padding(.horizontal)
+                    .background(
+                        Capsule()
+                            .foregroundColor(genderBackgroundColor)
+                    )
+            }
+            .padding(.top, 8)
+            .padding(.bottom)
+            .padding(.horizontal)
+        }
+    }
+}
+
+
